e9de5dcce900c89555d459d8ed37652612bc629f
Kyle Rose <kyle.rosefive5@gmail.com>
2021-10-29 15:57:54 -0400

    first commit

---
+++ scanner.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+# Port Scanner Script - give ip address and scan ports to determine if open
+import sys
+import socket
+from datetime import datetime
+
+# Define our target
+if len(sys.argv) == 2:
+	target = socket.gethostbyname(sys.argv[1]) #Translate hostname to IPv4
+else:
+	print("Invalid amount of arguments.")
+	print("Syntax: python3 scanner.py <ip>")
+	
+	#Add a banner
+print ("_" * 50)
+print ("Scanning target " + target)
+print ("Time started : " + str(datetime.now()))
+print ("_" * 50)
+
+try: 
+	for port in range(50,85):    #connect to port and make it IP4
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket.setdefaulttimeout(1)
+		result = s.connect_ex((target,port))  #Returns error indicator
+		print("Checking port {}".format(port))
+		if result == 0:
+			print("Port {} is open".format(port))
+		s.close()	
+		
+except KeyboardInterrupt:
+	print("\nExiting program")
+	sys.exit
+	
+except socket.gaierror:
+	print ("Hostname could not be resolved.")
+	sys.exit
+
+except socket.error:
+	print("Couldn't connect to server")
+	sys.exit
+
